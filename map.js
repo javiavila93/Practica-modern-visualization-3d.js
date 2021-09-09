69b9b447a55870cdd026303fdad9187f1f4ce89c
Javier Avila <cj.avilabermejo@gmail.com>
2021-09-09 20:23:04 +0200

    Práctica Finalizada y lista para entrega

--- map.js
+++ map.js
@@ -1,51 +1,55 @@
-//codigo js para mapa
 
-d3.json('practica_airbnb.json') //Carga de datos
+//Código js parala creación de las gráficas d3
+
+//Carga de datos e inicialización de función principal
+d3.json('practica_airbnb.json') 
     .then((featureCollection) => {
         drawMap(featureCollection);
     });
 
-//Función constructora del mapa
+//Función constructora del mapa y la gráfica
 function drawMap(featureCollection) {
     //variables tamañano para svg
     var width = 800;
     var height = 800;
 
-    var svg = d3.select('#map')
+    //Se crea svg y se inserta en el espacio div graphs
+    var svg = d3.select('#graphs')
         .append('svg')
         .attr('width', width)
         .attr('height', height)
         .append('g');
 
-    var center = d3.geoCentroid(featureCollection); //Encontrar la coordenada central del mapa (de la featureCollection)
+    // Se define el centro del mapa con geoCentroid
+    var center = d3.geoCentroid(featureCollection);
 
-    //var center_area = d3.geoCentroid(featureCollection.features[0]); //Encontrar la coordenada central de un area. (de un feature)
-
+    // se define projection y se ajusta al espacio definido
     var projection = d3.geoMercator()
         .fitSize([width, height], featureCollection)
         .center(center)
         .translate([width / 2, height / 2])
 
-
-    //Para crear paths a partir de una proyección
+    //se define la función generadora de path con geoPath
     var pathProjection = d3.geoPath().projection(projection);
     var features = featureCollection.features;
+    var barrio_madrid = featureCollection.features[0];
 
-    // Se añade el indice, color y avg_price con los datos de cada figura
+    //Se extraen los datos necesarios para las escalas en caso de no tener dato se pone por defecto 0
     features.forEach((d, i) => {
         d.idx = i;
-        d.avgprice = d.properties.avgprice
-        //d.avgbedrooms = d.properties.avgbedrooms;
+        d.avgprice = d.properties.avgprice || 0;
+        d.rooms = d.properties.avgbedrooms || 0;
         });
 
-//dominio para la escala de color
+    // Se definen la variables de dominio del color del mapa y la leyenda con extent
     var domainColor = d3.extent(features, function (d) {
         return d.avgprice;
     });
     var domainColorLegend = d3.extent(features, function (d) {
         return d.idx;
     });
-//Se crea la escala de color
+
+    // Se definen las escalas con el dominio anterior
     var scaleColor = d3.scaleSequential()
     .domain(domainColor)
     .range([0,1]);
@@ -54,45 +58,39 @@ function drawMap(featureCollection) {
     .domain(domainColorLegend)
     .range([0,1]);
 
+    // Se interpola los valores de los colores según los valores de avgprice para el mapa y idx para la leyenda
     features.forEach((d, i) => {
         d.color = d3.interpolateOrRd(scaleColor(d.avgprice));
         d.colorlegend = d3.interpolateOrRd(scaleColorLegend(d.idx));
-        //d.avgbedrooms = d.properties.avgbedrooms;
         });
 
+    //Se define el tooltip informativo a utilizar utilizando los estilos del css
     var tooltip = d3
-    .select("#map")
+    .select("#graphs")
     .append("div")
     .attr("class","tooltip")
     .style("padding", 5);
 
-
+    //Inicializamos la creación del mapa
     var createdPath = svg.selectAll('path')
     .data(features)
     .enter()
     .append('path')
     .attr("id", (d) => d.idx)
-    .attr('d', (d) => pathProjection(d))
-    .attr("opacity", function(d, i) {
-        d.opacity = 1;
-        return d.opacity})
-    .attr("fill", d => d.color)
-    .attr("stroke-width", 1)
-    .attr("stroke", "black")
-    .attr("stroke-dasharray", "1")
-    
+    .attr('d', (d) => pathProjection(d));
 
-
+// Se difine un valor por defecto para crear la leyenda
 var rectWidth = 5;
 
-  var legend =
-    d3.select("#map")
+// Se crea una leyenda y se definen sus paramétros
+var legend =
+    d3.select("#graphs")
     .append("div")
     .append("svg")
+    .attr("id", "legend")
     .attr("width", width)
     .attr("height", 100)
     .append("g")
-    //.attr("transform", "translate(50, 50)")
     .selectAll("rect")
     .data(features)
     .enter()
@@ -110,19 +108,38 @@ var rectWidth = 5;
         return d.colorlegend;
     })
     .attr("stroke-width", 1)
-    .on("mouseover", mouseOverLegend)
-    .on("click", showMap);
+    .on("mouseover", mouseOverLegend) //Función para tooltip en la leyenda
+    .on("mouseout", mouseOut); //Función para quitar el tooltip en la leyenda
 
-
-
+    //  Transición de entrada del mapa al iniciarla la página
     createdPath
-    .on("mouseover", mouseOverMap)
-    .on("click", clickColor)
-    /*.on("click", drawChart);*/
-    //.on("click", onlyZone); Zoom y centrar zona al hacer clic si da tiempo
-    function mouseOverMap (event, d) {
-    d3.select(this)
+    .transition("EnterMap")
+    .delay((d) => 100+100*d.idx)
+    .duration(500)
+    .attr("opacity", function(d, i) {
+        d.opacity = 1;
+        return d.opacity})
+    .attr("fill", d => d.color)
+    .attr("stroke-width", 1)
+    .attr("stroke", "black")
+    .attr("stroke-dasharray", "1");
+
+    createdPath.on("mouseover", mouseOverMap); //Función Tooltip para el mapa
+
+    createdPath.on("mouseout", mouseOut);//Ocultar el tooltip
+
+//Función Ocultar Tooltip
+function mouseOut (event, d) {
+    tooltip
     .transition()
+    .duration(200)
+    .style("visibility", "hidden")
+};
+
+// Función Mostrar Tooltip Mapa
+function mouseOverMap (event, d) {
+    d3.select(this)
+    .transition("tooltip")
     .duration(1000);
 
     tooltip
@@ -134,6 +151,7 @@ var rectWidth = 5;
     .text(`Barrio: ${d.properties.name} Precio Medio: ${d.avgprice}`); //Intentar poner multilínea
 };
 
+// Función Mostrar Tooltip Leyenda
 function mouseOverLegend (event, d) {
     d3.select(this)
     .transition()
@@ -148,73 +166,136 @@ function mouseOverLegend (event, d) {
     .text("Precio Medio -> Min: " + domainColor[0] + " Max: " + domainColor[1]); //Intentar poner multilínea
 };
 
-function clickColor (event, d) {
-    d3.selectAll("path")
-    .transition("hiddenMap")
-    .duration(500)
-    .attr("opacity", 0);
-    d3.select(this)
-    .transition("showZone")
-    .delay(100)
-    .duration(500)
-    .attr("opacity", 0.95);
-    }
+//Creación del svg para la gráfica de barras, se añade en el mismo Div, se crea un svg y se translada
+var svg_barchat = d3.select('#graphs')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height + 70)
+    .attr('transform','translate(' + width + ',' + -height + ')')
+    .append("g");
 
-function showMap (event, d) {
-    d3.selectAll("path").attr("opacity", 1);
-}
+//Inicializamos la gráfica
+drawBarchart(barrio_madrid.properties.avgbedrooms);
 
+//Función constructora de la gráfica
+function drawBarchart(avgbedrooms) {
+    var marginLeft = 50;
+    var marginTop = 70;
+    var width = 800/2
+    var height = 800 - marginTop;
+
+//Definimos las escalas
+var scaleX = d3.scaleBand().range([marginLeft, width]).padding(0.1);
+var scaleY = d3.scaleLinear().range([height, marginTop]);
+
+//Definimos el color de la escala
+var scaleColorBar = d3.scaleOrdinal(d3.schemePaired);
+
+//Definimos los Ejes
+var xAxis = d3.axisBottom(scaleX).tickSizeOuter(0);
+var yAxis = d3.axisLeft(scaleY);
+
+//Disporador para el cambio de gráfica según se clique en el mapa
+createdPath.on("click", redrawtransition)
+
+redraw(avgbedrooms);
+
+//Construimos previamente los ejes y los títulos
+svg_barchat
+    .append("g")
+    .attr("class", "axisY")
+    .attr("transform", `translate(${marginLeft})`)
+    .call(yAxis);
+
+svg_barchat
+    .append("g")
+    .attr("class", "axisX")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis);
+
+svg_barchat
+    .append("text")
+    .attr("transform", "translate(" + width/2 + "," + (height + 40) + ")")
+    .style("text-anchor", "middle")
+    .text("Nº de habitaciones");
+
+svg_barchat
+    .append("text")
+    .attr("y", 20)
+    .attr("x", marginLeft - 25)
+    .attr("dx", "1em")
+    .text("Nº de propiedades");
+
+//Función que recoge el dato idx para pintar la gráfica y llama a la función contructora
+function redrawtransition (event, d) {
+    redraw(d.properties.avgbedrooms);
 };
 
+//Función para la interactividad de la gráfica de barras
+function redraw(avgbedrooms) {
+var speed = 1000;
 
-/*function onlyZone(event, d) {
-    d3.selectAll(this)
-    .transition("showzoom")
-    .duration(0)
-    .delay(0)
-    .attr("opacity", function (d));
+//Definimos los dominos del mapa
+scaleX.domain(avgbedrooms.map((d) => d.bedrooms));
+scaleY.domain([0, d3.max(avgbedrooms, (d) => d.total)]);
 
-    d3.selectAll("path")
-    .transition("hidden")
-    .duration(200)
-    .delay(200)
-    .attr("opacity", 0);
-};*/
+//Borramos los rectangulos previos
+svg_barchat.selectAll("rect").remove();
+svg_barchat.selectAll(".recttext").remove();
 
+//Llamamos de nuevo a los ejes para que se adapten a la nueva gráfica
+svg_barchat.selectAll(".axisX").transition().duration(speed).call(xAxis);
+svg_barchat.selectAll(".axisY").transition().duration(speed).call(yAxis);
 
+//Introduccimos de nuevo las barras en la gráfica
+var rect = svg_barchat
+    .append("g")
+    .selectAll("rect")
+    .data(avgbedrooms)
+    .enter()
+    .append("rect")
+    .attr("x", (d) => scaleX(d.bedrooms))
+    .attr("y", height)
+    .attr("width", scaleX.bandwidth())
+    .attr("fill", (d) => scaleColorBar(d.bedrooms))
+    .attr("stroke", "black")
+    .attr("stroke-width", "1px");
 
+//Insertamos una transicción de abajo a arriba
+rect
+    .transition()
+    .duration(speed)
+    .ease(d3.easeLinear)
+    .attr("y", (d) => scaleY(d.total))
+    .attr("height", (d) => scaleY(0) - scaleY(d.total));
 
+//Imcluimos los textos de los cuadros
+var text = svg_barchat
+    .append("g")
+    .selectAll("text")
+    .data(avgbedrooms)
+    .enter()
+    .append("text")
+    .attr("class", "recttext")
+    .text((d) => d.total)
+    .attr("x", function (d) {
+        var textLength = this.getComputedTextLength();
+        return scaleX(d.bedrooms) + scaleX.bandwidth() / 2 - textLength / 2;
+    })
+    .attr("y", height)
+    .attr("fill", "black");
 
-/*     createdPath.on('click', function(event, d) {
-        d.opacity = d.opacity ? 0 : 1;
-        d3.select(this).attr('opacity', d.opacity););
-    })*/
+//Colocamos por encima de la rectangulos sus correspondientes textos para
+text
+    .transition()
+    .duration(speed)
+    .ease(d3.easeLinear)
+    .attr("y", (d) => {
+        var borderTop = 15;
+        var y = scaleY(d.total) - borderTop;
+        return y > height ? height : y;
+    });
+    }
 
-    //var scaleColor = d3.scaleLinear().range(["white", "red"]);
-   //createdPath.attr('fill', (d) => scaleColor(d.value));
-
-
-
-    //Creacion de una leyenda
-    /* var nblegend = 10;
-    var widthRect = (width / nblegend) - 2;
-    var heightRect = 10;
-
-    var scaleLegend = d3.scaleLinear()
-        .domain([d3.min(featureCollection, function (d) {return d.avgprice}), d3.max(featureCollection, function (d) {return d.avgprice})]);
-
-
-    var text_legend = svg.append("g")
-        .selectAll("text")
-        .data(d3.schemeTableau10)
-        .enter()
-        .append("text")
-        .attr("x", (d, i) => scaleLegend(i)) // o (i * (widthRect + 2))
-        .attr("y", heightRect * 2.5)
-        .text((d) => d)
-        .attr("font-size", 12)
-}
-
-Si quisiesemos unir  dos archivos Geojson
-npm install -g topojson (Primero instalar node.js) => solo para uniones
-topojson spain.json canarias.json -o full_spain.json*/
+};
+};
